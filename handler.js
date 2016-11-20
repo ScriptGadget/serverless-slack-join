782ff851b41f1e630843616be5bc8ec073f646ce
Bill Glover <bill@billglover.com>
2016-11-20 07:21:36 +0000

    Refactor a bit and add more comments.

--- handler.js
+++ handler.js
@@ -1,40 +1,21 @@
-'use strict';
-var request = require('request');
-var qs = require('qs');
-require('dotenv').config();
+// handler.js - The main Lambda function
+//
+// All of the real work is done in the library files to make testing
+// and porting a little easier, but this needs work still.
+//
 
-var config = {
-  slack_domain: process.env.slack_domain, 
-  slack_token: process.env.slack_token 
-};
+'use strict';
+var qs = require('qs');
+var reply = require('./lib/reply');
+var slack = require('./lib/slack'); 
 
 module.exports.join = (event, context, callback) => {
+  // AWS API Gateway doesn't really get form data.
+  // We could use a template here, but this is alot less opaque.
   var params = qs.parse(event.body);
-  if (! params.email ) {
-    callback(null, { statusCode: 200,
-                     body: JSON.stringify({
-                       error : 'email addess is required',
-                     }),
-                   });
-  } else {
-    console.log("in join: " + event.email);
-    request.post({
-        url: 'https://'+ config.slack_domain + '/api/users.admin.invite',
-        form: {
-          email: params.email,
-          token: config.slack_token,
-          set_active: true
-        }
-    }, function(err, httpResponse, body) {
-      if (err) {
-        callback(null, err);
-      } else {
-        callback(null, { statusCode: 200,
-                         body: JSON.stringify({
-                           message : 'Invitation sent.'
-                         }),
-                       });
-      }
-    });
-  };
+  
+  if (! params.email) {
+    return reply.reply(200, { error: "email address is required" }, callback);
+  }
+  return slack.join(params, callback);
 };
